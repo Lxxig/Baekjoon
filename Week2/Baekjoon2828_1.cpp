634404e5cd27d506faf345ba834892bb1662e0d8
Lxxig <mayong0707@gmail.com>
2025-01-03 19:30:09 +0900

    1992, 2828, 2910, 4659

--- Week2/Baekjoon2828_1.cpp
+++ Week2/Baekjoon2828_1.cpp
@@ -1,7 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, m, l, r, b, ret, j;
+
 int main() 
 {
-	
+	cin >> n >> m;
+	cin >> j;
+	l = 1; r = m;
+	while (j--)
+	{
+		cin >> b;
+		if (b > r)
+		{
+			int s = b - r;
+			r += s;
+			l += s;
+			ret += s;
+		}
+		else if (b<=r && b>=l)
+		{
+			continue;
+		}
+		else if (b < l)
+		{
+			int s = l - b;
+			r -= s;
+			l -= s;
+			ret += s;
+		}
+	}
+	std::cout << ret << "\n";
 }
